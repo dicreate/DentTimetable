3ef2e724d4aeb22391981624f99bfc3876d75611
DmitriyKovchugo <dkovchugo@mail.ru>
2023-05-15 18:38:22 +0300

    feat: add AddPatientScreen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen, PatientScreen, AddPatientsScreen } from "./screens";
-
+import { NativeBaseProvider } from "native-base";
 
 
 export default function App() {
@@ -10,46 +10,48 @@ export default function App() {
   const Stack = createNativeStackNavigator();
   
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="Home" 
-          component={ HomeScreen }
-          options={{ 
-            title: 'Пациенты', 
-            headerTintColor: '#2A86FF',
-            headerStyle: {
-              evelation: 0.8,
-              shadowOpacity: 0.8, 
-            }
-          }}
-          />
-        <Stack.Screen 
-          name="Patient" 
-          component={ PatientScreen } 
-          options={{
-            title: 'Карта пациента',
-            headerTintColor: '#2A86FF',
-            headerStyle: {
-              evelation: 0.8,
-              shadowOpacity: 0.8, 
-            }
-          }}
-          />
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
           <Stack.Screen 
-          name="AddPatient" 
-          component={ AddPatientsScreen } 
-          options={{
-            title: 'Добавить пациента',
-            headerTintColor: '#2A86FF',
-            headerStyle: {
-              evelation: 0.8,
-              shadowOpacity: 0.8, 
-            }
-          }}
-          />
-      </Stack.Navigator>
-    </NavigationContainer>
+            name="Home" 
+            component={ HomeScreen }
+            options={{ 
+              title: 'Пациенты', 
+              headerTintColor: '#2A86FF',
+              headerStyle: {
+                evelation: 0.8,
+                shadowOpacity: 0.8, 
+              }
+            }}
+            />
+          <Stack.Screen 
+            name="Patient" 
+            component={ PatientScreen } 
+            options={{
+              title: 'Карта пациента',
+              headerTintColor: '#2A86FF',
+              headerStyle: {
+                evelation: 0.8,
+                shadowOpacity: 0.8, 
+              }
+            }}
+            />
+            <Stack.Screen 
+            name="AddPatient" 
+            component={ AddPatientsScreen } 
+            options={{
+              title: 'Добавить пациента',
+              headerTintColor: '#2A86FF',
+              headerStyle: {
+                evelation: 0.8,
+                shadowOpacity: 0.8, 
+              }
+            }}
+            />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
--- screens/AddPatientsScreen.js
+++ screens/AddPatientsScreen.js
@@ -1,15 +1,75 @@
-import React from 'react'
-import { Text,  View } from 'react-native'
+import React, { useState } from 'react'
+import { Text,  View, SafeAreaView, TextInput, StyleSheet} from 'react-native'
 import { LogBox } from 'react-native';
+import { Input, Stack, Button } from "native-base";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import styled from 'styled-components'
 
 function AddPatientsScreen () {
 
+  const [values, setValues] = useState({});
+
+  const hangeChange = (name, e) => {
+    
+    const text = e.nativeEvent.text;
+
+    setValues({
+      ...values, 
+      [name]: text
+    });
+  }
+
   return (
-   <View style = {{flex: 1}}>
-      <Text>Form</Text>   
+   <View style = {{flex: 1, marginTop: 50, }}>
+      
+      <Stack space={5} w="75%" maxW="300px" mx="auto">
+        <Input 
+          value = {values.fullname} 
+          onChange = {hangeChange.bind(this, 'fullname')}
+          autoFocus 
+          variant="underlined" 
+          size="md" 
+          placeholder="Имя и фамилия" w="100%" 
+        />
+
+        <Input 
+          value = {values.phone} 
+          dataDetectorTypes = {"phoneNumber"} 
+          inputMode = {"tel"}
+          variant="underlined" 
+          size="md" 
+          placeholder="Номер телефона" 
+          w="100%" 
+        />
+        
+        <ButtonView>
+          <Button
+          onPress={() => console.log("hello world")} 
+          size="md"
+          w="100%" 
+          borderRadius={'20px'} 
+          colorScheme="green" 
+          >
+            <ButtonText>
+              <Ionicons name="ios-add" size={20} color="white" />
+              Добавить
+            </ButtonText>  
+          </Button>
+        </ButtonView>
+      </Stack>
    </View>
   )
 }
+
+const ButtonView = styled.View`
+  margin-top: 30px;
+`
+
+const ButtonText = styled.Text`
+  flex-direction: row;
+  color: white;
+  font-size: 16px;
+`
 
 LogBox.ignoreLogs([
    'Non-serializable values were found in the navigation state',
