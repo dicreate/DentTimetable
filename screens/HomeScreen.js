3707a3fb7603c49e4fcc9f786825620e0db8a2c9
DmitriyKovchugo <dkovchugo@mail.ru>
2023-06-12 01:36:07 +0300

    feat: add refreshing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,218 +8,18 @@ import { appoitmentsApi, patientsApi } from '../utils/api'
 
 const HomeScreen = ({navigation}) => {
 
-/*   const DATA = [
-    {
-      title: '14 сентября',
-      data: [ {
-        time: '15:30',    
-        active: 'true',
-        patient: {
-          fullname: 'Карина Кузьмич',
-          avatar: 'https://sun2.ncot-by-minsk.userapi.com/s/v1/ig2/uG4QK3ph3JTqYhpuDLhutf3jshNI4CXOA6arMlADMKCWvME5Ch4LjOoOyD7u4RwDd8GH9lz-pe6NF7jL_8cLYdiA.jpg?size=100x100&quality=95&crop=1,70,759,759&ava=1',
-          diagnosis: 'Обследование',
-          phone: +3752932932498
-        }
-      },
-      {
-        time: '17:00',    
-        patient: {
-          fullname: 'Дмитрий Ковчуго',
-          avatar: 'https://sun9-48.userapi.com/impg/MhOphtIiXF_ExSjqmqKrPPwnN__V6eorDKCBUg/U-lPP154VH4.jpg?size=810x1080&quality=96&sign=ca48260be27a14ec221edf114e609803&type=album',
-          diagnosis: 'Лечение зуба',
-          phone: +3752932932498
-        }
-      },],
-    },
-    {
-      title: '16 сентября',
-      data: [{
-        time: '09:00',    
-        patient: {
-          fullname: 'Данателло Пупкин',
-          avatar: 'https://sun1.ncot-by-minsk.userapi.com/s/v1/ig2/bHN5Mwcp3lxpjVwV46r_XtSuZN6Oad8LMGEHDOqmLLgu9CTh-3FnwZ7bqglZVCeEbx-Emp1-buQA5w5vCrOsEe6P.jpg?size=100x100&quality=96&crop=482,179,275,275&ava=1',
-          diagnosis: 'Пульпит',
-          phone: +3752932932498
-        }
-      }],
-    },
-    {
-      title: '17 сентября',
-      data: [{
-        time: '10:00',    
-        patient: {
-          fullname: 'Данателло Смирнов',
-          avatar: 'https://sun1.ncot-by-minsk.userapi.com/s/v1/ig2/bHN5Mwcp3lxpjVwV46r_XtSuZN6Oad8LMGEHDOqmLLgu9CTh-3FnwZ7bqglZVCeEbx-Emp1-buQA5w5vCrOsEe6P.jpg?size=100x100&quality=96&crop=482,179,275,275&ava=1',
-          diagnosis: 'Пульпит',
-          phone: +3752932932498
-        },
-      },
-      {
-        time: '11:00',    
-        patient: {
-          fullname: 'Данателло Смирнов',
-          avatar: 'https://sun1.ncot-by-minsk.userapi.com/s/v1/ig2/bHN5Mwcp3lxpjVwV46r_XtSuZN6Oad8LMGEHDOqmLLgu9CTh-3FnwZ7bqglZVCeEbx-Emp1-buQA5w5vCrOsEe6P.jpg?size=100x100&quality=96&crop=482,179,275,275&ava=1',
-          diagnosis: 'Пульпит',
-          phone: +3752932932498
-        },
-      },
-      {
-        time: '12:00',    
-        patient: {
-          fullname: 'Данателло Смирнов',
-          avatar: 'https://sun1.ncot-by-minsk.userapi.com/s/v1/ig2/bHN5Mwcp3lxpjVwV46r_XtSuZN6Oad8LMGEHDOqmLLgu9CTh-3FnwZ7bqglZVCeEbx-Emp1-buQA5w5vCrOsEe6P.jpg?size=100x100&quality=96&crop=482,179,275,275&ava=1',
-          diagnosis: 'Пульпит',
-          phone: +3752932932498
-        },
-      },
-      {
-        time: '13:00',    
-        patient: {
-          fullname: 'Данателло Смирнов',
-          avatar: 'https://sun1.ncot-by-minsk.userapi.com/s/v1/ig2/bHN5Mwcp3lxpjVwV46r_XtSuZN6Oad8LMGEHDOqmLLgu9CTh-3FnwZ7bqglZVCeEbx-Emp1-buQA5w5vCrOsEe6P.jpg?size=100x100&quality=96&crop=482,179,275,275&ava=1',
-          diagnosis: 'Пульпит',
-          phone: +3752932932498
-        },
-      },
-      {
-        time: '14:00',    
-        patient: {
-          fullname: 'Данателло Смирнов',
-          avatar: 'https://sun1.ncot-by-minsk.userapi.com/s/v1/ig2/bHN5Mwcp3lxpjVwV46r_XtSuZN6Oad8LMGEHDOqmLLgu9CTh-3FnwZ7bqglZVCeEbx-Emp1-buQA5w5vCrOsEe6P.jpg?size=100x100&quality=96&crop=482,179,275,275&ava=1',
-          diagnosis: 'Пульпит',
-          phone: +3752932932498
-        },
-      },
-      {
-        time: '15:00',    
-        patient: {
-          fullname: 'Данателло Смирнов',
-          avatar: 'https://sun1.ncot-by-minsk.userapi.com/s/v1/ig2/bHN5Mwcp3lxpjVwV46r_XtSuZN6Oad8LMGEHDOqmLLgu9CTh-3FnwZ7bqglZVCeEbx-Emp1-buQA5w5vCrOsEe6P.jpg?size=100x100&quality=96&crop=482,179,275,275&ava=1',
-          diagnosis: 'Пульпит',
-          phone: +3752932932498
-        },
-      }],
-    },
-  ]; */
-
-  const DATA = [
-    {
-        "title": "19.06.2023",
-        "data": [
-            {
-                "_id": "64835a243a82938d7c943f95",
-                "patient": {
-                    "_id": "6451fae3163322db3563a575",
-                    "fullname": "TEST",
-                    "phone": "123456789",
-                    "createdAt": "2023-05-03T06:10:43.832Z",
-                    "updatedAt": "2023-05-03T06:10:43.832Z",
-                    "__v": 0
-                },
-                "dentNumber": 30,
-                "diagnosis": "По приколу ",
-                "price": 1500,
-                "date": "19.06.2023",
-                "time": "14:00",
-                "createdAt": "2023-06-09T16:58:12.213Z",
-                "updatedAt": "2023-06-09T16:58:12.213Z",
-                "__v": 0
-            },
-            {
-                "_id": "64835a253a82938d7c943f98",
-                "patient": {
-                    "_id": "6451fae3163322db3563a575",
-                    "fullname": "TEST",
-                    "phone": "123456789",
-                    "createdAt": "2023-05-03T06:10:43.832Z",
-                    "updatedAt": "2023-05-03T06:10:43.832Z",
-                    "__v": 0
-                },
-                "dentNumber": 30,
-                "diagnosis": "По приколу ",
-                "price": 1500,
-                "date": "19.06.2023",
-                "time": "14:00",
-                "createdAt": "2023-06-09T16:58:13.257Z",
-                "updatedAt": "2023-06-09T16:58:13.257Z",
-                "__v": 0
-            },
-            {
-                "_id": "6483645d4782c4462b6fbc09",
-                "patient": {
-                    "_id": "6451fae3163322db3563a575",
-                    "fullname": "TEST",
-                    "phone": "123456789",
-                    "createdAt": "2023-05-03T06:10:43.832Z",
-                    "updatedAt": "2023-05-03T06:10:43.832Z",
-                    "__v": 0
-                },
-                "dentNumber": 30,
-                "diagnosis": "По приколу ",
-                "price": 1500,
-                "date": "19.06.2023",
-                "time": "14:00",
-                "createdAt": "2023-06-09T17:41:49.438Z",
-                "updatedAt": "2023-06-09T17:41:49.438Z",
-                "__v": 0
-            },
-            {
-                "_id": "6483645e4782c4462b6fbc0c",
-                "patient": {
-                    "_id": "6451fae3163322db3563a575",
-                    "fullname": "TEST",
-                    "phone": "123456789",
-                    "createdAt": "2023-05-03T06:10:43.832Z",
-                    "updatedAt": "2023-05-03T06:10:43.832Z",
-                    "__v": 0
-                },
-                "dentNumber": 30,
-                "diagnosis": "По приколу ",
-                "price": 1500,
-                "date": "19.06.2023",
-                "time": "14:00",
-                "createdAt": "2023-06-09T17:41:50.598Z",
-                "updatedAt": "2023-06-09T17:41:50.598Z",
-                "__v": 0
-            }
-        ]
-    },
-    {
-        "title": "20.06.2023",
-        "data": [
-            {
-                "_id": "648369bb98c8562d57ca15a2",
-                "patient": {
-                    "_id": "6451fae3163322db3563a575",
-                    "fullname": "TEST",
-                    "phone": "123456789",
-                    "createdAt": "2023-05-03T06:10:43.832Z",
-                    "updatedAt": "2023-05-03T06:10:43.832Z",
-                    "__v": 0
-                },
-                "dentNumber": 30,
-                "diagnosis": "По приколу ",
-                "price": 1500,
-                "date": "20.06.2023",
-                "time": "14:00",
-                "createdAt": "2023-06-09T18:04:43.862Z",
-                "updatedAt": "2023-06-09T18:04:43.862Z",
-                "__v": 0
-            }
-        ]
-    }
-]
-
-
   const [appointmentsData, setAppointmentsData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await appoitmentsApi.get()
-
-      setAppointmentsData(response.data.data)
-    }
-    fetchData();
+  const fetchAppointments = async () => {
+    setIsLoading(true);
+    const response = await appoitmentsApi.get()
+    setAppointmentsData(response.data.data)
+    setIsLoading(false);
+  }
+  
+  useEffect(() => { 
+    fetchAppointments();
   }, [])
 
     
@@ -231,7 +31,8 @@ const HomeScreen = ({navigation}) => {
          <SectionList
             sections={appointmentsData }
             keyExtractor={(item, index) => index}
-            refreshing = {true}
+            onRefresh={fetchAppointments}
+            refreshing = {isLoading}
             renderItem={({item}) => <Appoitment item = {item} navigate = {navigation.navigate}/>}
             renderSectionHeader={({section: {title}}) => (
               <SectionTitle> {title}</SectionTitle>
