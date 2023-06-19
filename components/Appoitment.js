6379d6a802e2a7d4a7af7fd55b958fae8303cca6
Dmitriy <dkovchugo@mail.ru>
2023-06-19 17:41:53 +0300

    feat: work with actionsheet in Appoitment

--- components/Appoitment.js
+++ components/Appoitment.js
@@ -1,33 +1,58 @@
+import React, { useRef } from 'react'
 import { Text, View, TouchableWithoutFeedback } from 'react-native';
 import styled from 'styled-components/native';
 import { default as GrayText } from './GrayText'
 import { default as Badge } from './Badge'
 import getAvatarColor from '../utils/getAvatarColor';
+import ActionSheet from 'react-native-actionsheet';
 
 const Appoitment = ({item, navigate}) => {
    const { patient, active, time } = item;
 
    const avatarColors = getAvatarColor(patient.fullname[0].toUpperCase())
 
+   let actionSheet = useRef();
+   let optionArray = [
+      'Option 1', 'Option 2', 'Cancel'
+   ];
+ 
+   const showActionSheet = () => {
+      actionSheet.current.show();
+      useNativeDriver: true
+   }
+
 return (
-      <AppoitmentItem onLongPress={() => console.log('long-long press')} onPress = { navigate.bind(this, 'Patient', {item}) }>
-         <Avatar style = {{
-            backgroundColor: avatarColors.background,
-         }}>
-            <Letter style = {{
-               color: avatarColors.color,
-            }} > 
-               { patient.fullname[0].toUpperCase() }
-            </Letter>
-         </Avatar>
-         <View style= {{style: 'flex: 1'}}>
-            <FullName>{patient.fullname}</FullName>
-            <GrayText>{patient.diagnosis}</GrayText>
-         </View>
-         <Badge active = {active}>
-            {time}
-         </Badge>
-      </AppoitmentItem>
+      <>
+         <AppoitmentItem onLongPress={showActionSheet} onPress = { navigate.bind(this, 'Patient', {item}) }>
+            <Avatar style = {{
+               backgroundColor: avatarColors.background,
+            }}>
+               <Letter style = {{
+                  color: avatarColors.color,
+               }} > 
+                  { patient.fullname[0].toUpperCase() }
+               </Letter>
+            </Avatar>
+            <View style= {{style: 'flex: 1'}}>
+               <FullName>{patient.fullname}</FullName>
+               <GrayText>{patient.diagnosis}</GrayText>
+            </View>
+            <Badge active = {active}>
+               {time}
+            </Badge>
+         </AppoitmentItem>
+         <ActionSheet 
+            ref = {actionSheet}
+            title = {'title'}
+            options = {optionArray}
+            cancelButtonIndex = {2}
+            destructiveButtonIndex={0}
+            onPress={(index) => {
+               alert(optionArray[index]);
+            }}
+         />
+      </>
+      
    );
 }
 
