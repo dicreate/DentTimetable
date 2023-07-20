8ea6968660e6223d12ad656bfcfb072a017484b5
Dmitriy <dkovchugo@mail.ru>
2023-07-20 15:42:02 +0300

    feat: add auto update AddPatient

--- screens/AddPatientsScreen.js
+++ screens/AddPatientsScreen.js
@@ -25,7 +25,7 @@ function AddPatientsScreen ({navigation}) {
 
   const onSumbit = () => {
     patientsApi.add(values).then(() => {
-      navigation.navigate('Patients');
+      navigation.navigate('Patients', { lastUpdatePatient: new Date() } );
     }).catch(() => alert("Минимальное количество символов для полей 4"));
   }
 
