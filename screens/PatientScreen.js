60781555814b7f42f8aabe77fdbc28a1900610ac
DmitriyKovchugo <dkovchugo@mail.ru>
2023-10-19 21:30:02 +0300

    feat: add info for patient

--- screens/PatientScreen.js
+++ screens/PatientScreen.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { Text,  View, ActivityIndicator, Linking, ScrollView } from 'react-native'
+import { Text,  View, ActivityIndicator, Linking, ScrollView, StyleSheet, Modal, TouchableOpacity } from 'react-native'
 import styled from 'styled-components'
 import { GrayText, Button, Badge, PlusButton } from '../components'
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
-import { Foundation } from '@expo/vector-icons';
-import { Fontisto } from '@expo/vector-icons'; 
+import { Foundation, Fontisto, MaterialIcons } from '@expo/vector-icons'; 
 import moment from 'moment/moment';
 import 'moment/locale/ru';
 import { getPatientAppointments } from '../sqlite/requests';
@@ -13,8 +12,11 @@ const PatientScreen = ({ navigation, route }) => {
 
   const { item } = route.params;
 
+  console.log(item)
+
   const [ appointments, setAppointments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [openInfo, setOpenInfo] = useState(false);
 
   const getAppointments = async () => {
     const appointmentsTable = await getPatientAppointments(item.patientId);
@@ -29,7 +31,7 @@ const PatientScreen = ({ navigation, route }) => {
   }, [])
   
   return (
-    <View style = {{flex: 1}}>
+    <View style = {{flex: 1, backgroundColor: '#f8fafd'}}>
       <ScrollView>
         <PatientDetails>
           <PatientContacts>
@@ -58,9 +60,7 @@ const PatientScreen = ({ navigation, route }) => {
                 Формула зубов
               </Button>
               <Button
-                onPress = {() => navigation.navigate('Formula', {
-                patientId: item.patientId
-              })}>
+                onPress = {() => setOpenInfo(true)}>
                 Информация
               </Button>
             </ButtonView>
@@ -76,24 +76,24 @@ const PatientScreen = ({ navigation, route }) => {
             : appointments !== 'no appointments'
               ? appointments.map((appointment) => 
                 <AppoitmentCard key = {appointment.id}>
-                <AppoitmentCardRow>
+                <CardRow>
                   <IconContainer><MaterialCommunityIcons name="tooth-outline" size={24} color="#A3A3A3" /></IconContainer>
                   <AppoitmentCardLabel>Зуб:<Text style={{fontWeight: 'bold'}}> {appointment.toothNumber ? appointment.toothNumber : 'не указан'}</Text></AppoitmentCardLabel>
-                </AppoitmentCardRow>
-                <AppoitmentCardRow>
+                </CardRow>
+                <CardRow>
                   <IconContainer><Foundation name="clipboard-notes" size={24} color="#A3A3A3" /></IconContainer>
                   <AppoitmentCardLabel>Диагноз:<Text style={{fontWeight: 'bold'}}> {appointment.diagnosis ? appointment.diagnosis : 'не указан'}</Text></AppoitmentCardLabel>
-                </AppoitmentCardRow>
-                <AppoitmentCardRow>
+                </CardRow>
+                <CardRow>
                   <IconContainer><Fontisto name="injection-syringe" size={24} color="#A3A3A3" /></IconContainer>         
                   <AppoitmentCardLabel>Анестезия:<Text style={{fontWeight: 'bold'}}> {appointment.anesthetization ? 'Да' : 'Нет'}</Text></AppoitmentCardLabel>
-                </AppoitmentCardRow>  
-                <AppoitmentCardRow 
+                </CardRow>  
+                <CardRow 
                 style = {{ marginTop: 15, justifyContent: 'space-between' }}
                 >
                   <Badge style = {{ width: 155, marginLeft: 0 }} active>{moment(appointment.date).locale('ru').format('DD.MM.YYYY')} - {appointment.time}</Badge>
                   <Badge color='green'>{appointment.price}</Badge>    
-                </AppoitmentCardRow>   
+                </CardRow>   
               </AppoitmentCard>)
               : null 
           }
@@ -103,10 +103,55 @@ const PatientScreen = ({ navigation, route }) => {
       <PlusButton onPress = {() => navigation.navigate('AddAppointment', {
         patientId: item.id
       })}/>
+       <Modal
+          animationType='slide'
+          transparent={true}
+          visible={openInfo}
+        >
+          <View style = {styles.centeredView}>
+            <View style = {styles.modalView}>
+              <CardRow>
+                  <IconContainer><MaterialIcons name="smoking-rooms" size={24} color="#A3A3A3" /></IconContainer>         
+                  <AppoitmentCardLabel>Курение:<Text style={{fontWeight: 'bold'}}> {item.isSmoking ? "да" : "нет"}</Text></AppoitmentCardLabel>
+              </CardRow>
+              <CardRow>
+                  <IconContainer><MaterialIcons name="pregnant-woman" size={24} color="#A3A3A3" /></IconContainer>         
+                  <AppoitmentCardLabel>Беременность:<Text style={{fontWeight: 'bold'}}> {item.isPregnancy ? "да" : "нет"}</Text></AppoitmentCardLabel>
+              </CardRow>    
+              <TouchableOpacity style = {{marginTop: 10, alignSelf: "center"}} onPress={() => {
+                  setOpenInfo(false)   
+                }}>
+                  <Text>Закрыть</Text>
+              </TouchableOpacity>
+            </View>
+         </View>
+      </Modal>
       </View>
   )
 }
 
+const styles = StyleSheet.create({
+  centeredView: {
+   alignItems: 'center',
+   justifyContent: 'center',
+   height: '100%'
+ },
+ modalView: {
+   backgroundColor: 'white',
+   borderRadius: 20,
+   width: '80%',
+   maxWidth: 400,
+   padding: 35,
+   shadowColor: '#000',
+   shadowOffset: {
+     width: 0,
+     height: 2,
+   },
+   shadowOpacity: 0.25, 
+   shadowRadius: 4,
+   elevation: 5,
+ }
+})
 
 const PatientContacts = styled.View`
   display: flex;
@@ -119,7 +164,7 @@ const IconContainer = styled.View`
   align-items: center;
 `
 
-const AppoitmentCardRow = styled.View`
+const CardRow = styled.View`
   flex-direction: row;
   align-items: center;
   gap: 5px;
@@ -157,7 +202,6 @@ const PatientDetails = styled(Container)`
 
 const PatientAppoitments = styled.View`
   flex: 1;
-  background: #f8fafd;
 `;
 
 const ButtonView = styled.View`
